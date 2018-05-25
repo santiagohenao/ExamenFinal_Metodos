edc30e93b68826d3a631540e0d7a0b780f293f20
santiagohenao-e-enfinal_metodos-n8m7c3oe <santiagohenao-e-enfinal_metodos-n8m7c3oe@local>
2018-05-25 00:33:09 +0000

    Terminado ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -8,5 +8,12 @@ import numpy as np
 x = np.linspace(0,2.,10)
 fx = np.array([0., 0.0494, 0.1975, 0.4444, 0.7901,1.2346 , 1.7778, 2.4198, 3.1605, 4.])
 
+def desplazar(numpy_arr):
+    lista=[0]+list(numpy_arr)
+    return np.array(lista[1:-1])
 
+dx=np.mean(x[1:]-desplazar(x))
 
+df=(fx[1:]-desplazar(fx))/dx # valores de la derivada desde donde está definida, es decir, desde x[1]
+
+df2=(df[1:]-desplazar(df))/dx # la segunda derivada sólo está definida desde x[2]
