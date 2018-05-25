36504ad14d6b7ebb9c3607f66d66bad10fb6f9de
santiagohenao-e-enfinal_metodos-n8m7c3oe <santiagohenao-e-enfinal_metodos-n8m7c3oe@local>
2018-05-25 00:45:00 +0000

    Terminado ejercicio 3

--- ejercicio3.py
+++ ejercicio3.py
@@ -7,3 +7,26 @@ import numpy as np
 t = np.array([0.,0.1,0.2,0.3,0.4,0.5,0.6, 0.8, 0.9])
 u = np.array([12.,45.,6.,78.,34.,22.,-10.,31.,-27.])
 v = np.array([3.,11.,1.3,37.,11.,6.,-23.,7.,7.])
+
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10,7))
+plt.scatter(t,u,s=20,label="u")
+plt.scatter(t,v,s=20,label="v")
+plt.plot(t,u)
+plt.plot(t,v)
+plt.legend(loc=4)
+plt.savefig("serie.png")
+
+def cov(a,b):
+    if(len(a)!=len(b)):
+        return 1j
+    c=0
+    for i in range(len(a)):
+        c+=(a[i]-a.mean())*(b[i]-b.mean())
+    return c/(len(a)-1)
+
+print(cov(u,v))
